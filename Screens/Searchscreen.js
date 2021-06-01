1efcfbf670db4e6417e1731cb4d36fd669c50a9e
Anirudh-030307 <65300694+Anirudh-030307@users.noreply.github.com>
2021-06-01 15:58:33 +0530

    Add files via upload

---
+++ Screens/Searchscreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Searchscreen extends React.Component {
+  render() {
+    return (
+      <View style={{ matgin: 50 }}>
+        <Text>SEARCH</Text>
+      </View>
+    );
+  }
+}
